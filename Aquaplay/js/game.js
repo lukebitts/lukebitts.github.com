f158ad302238d7346c07f67f5f4ea70ea0073c1c
Lucas Bittencourt <lbittencs@gmail.com>
2013-03-07 21:15:20 -0300

    Touch corrigido

--- Aquaplay/js/game.js
+++ Aquaplay/js/game.js
@@ -3,7 +3,7 @@ function main() {
 	new LoadingScreen(stage);
 	
 	stage.enableMouseOver(1000);
-	createjs.Touch.enable(this.stage);
+	createjs.Touch.enable(stage);
 	
 	var tick = function(evt) {
 		stage.update(evt);
