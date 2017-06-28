5c51388cfec03d3b95803b29b8587af6cb6b3e64
Lucas Bittencourt <lbittencs@gmail.com>
2017-06-28 15:34:38 -0300

    CPF crate documentation

---
+++ docs/cpf/cpf/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["validate_cpf","Checa se a `string` de input é uma representação válida de um CPF. O input deve conter exatamente 11 dígitos e nenhum caractere especial."]]});
---
+++ docs/cpf/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["cpf"] = {"doc":"Uma pequena biblioteca para validação de CPFs","items":[[5,"validate_cpf","cpf","Checa se a `string` de input é uma representação válida de um CPF. O input deve conter exatamente 11 dígitos e nenhum caractere especial.",null,{"inputs":[{"name":"s"}],"output":{"name":"bool"}}]],"paths":[]};
+initSearch(searchIndex);
